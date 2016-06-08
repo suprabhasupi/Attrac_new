5dd053021e8dd7670761f40a6f8313c834e42b7a
suprabhasupi <suprabhasupi@gmail.com>
2016-06-08 18:06:44 +0530

    menus_box

---
+++ includes/edit_content.php
@@ -0,0 +1,104 @@
+<link rel="stylesheet" type="text/css" href="css/menu_new.css">
+<div class="row col-md-12 ">
+	
+<div class="col-md-3">
+	<i class="fa fa-pencil" aria-hidden="true"></i> <span class="edit tregular">Edit</span>
+
+</div>
+
+
+</div>
+<div class="row col-md-12 ">&nbsp;</div>
+<div class="row col-md-12 col-xs-12 col-sm-12">
+	
+	<div class="col-md-1 col-xs-1 col-sm-1"></div>
+
+	<div class="col-md-10 col-xs-10 col-sm-10">
+		<div class="col-md-12 col-xs-12 col-sm-12">
+		
+		<div class="row col-md-offset-12 ">&nbsp;</div>
+			
+			<div class="col-md-12 tregular">
+            <div class="col-md-3">
+                <div style="height: 0; width: 0; position: absolute; visibility: hidden;" aria-hidden="true">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" focusable="false">
+                        <symbol id="ripply-scott2" viewBox="0 0 100 100">
+                            <circle id="ripple-shape" cx="1" cy="1" r="1" />
+                        </symbol>
+                    </svg>
+                </div>
+                <div class="content">
+                    <!-- button component -->
+
+                    <button id="js-ripple-btn2" class="button form_box styl-material">
+                     <img src="menu_images/icon1.png">
+                       <p class="icon_cont"> EDIT STUDENT LIST</p>
+                        <svg class="ripple-obj" id="js-ripple2">
+                            <use height="100" width="100" xlink:href="#ripply-scott2" class="js-ripple2"></use>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+             <div class="col-md-3">
+                <div style="height: 0; width: 0; position: absolute; visibility: hidden;" aria-hidden="true">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" focusable="false">
+                        <symbol id="ripply-scott2" viewBox="0 0 100 100">
+                            <circle id="ripple-shape" cx="1" cy="1" r="1" />
+                        </symbol>
+                    </svg>
+                </div>
+                <div class="content">
+                    <!-- button component -->
+                    <button id="js-ripple-btn2" class="button form_box styl-material btn_edit1" >
+                     <img src="menu_images/icon2.png">
+
+                    <p class="icon_cont">    EDIT FACULTY LIST</p>
+                        <svg class="ripple-obj" id="js-ripple2">
+                            <use height="100" width="100" xlink:href="#ripply-scott2" class="js-ripple2"></use>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+             <div class="col-md-3">
+                <div style="height: 0; width: 0; position: absolute; visibility: hidden;" aria-hidden="true">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" focusable="false">
+                        <symbol id="ripply-scott2" viewBox="0 0 100 100">
+                            <circle id="ripple-shape" cx="1" cy="1" r="1" />
+                        </symbol>
+                    </svg>
+                </div>
+                <div class="content">
+                    <!-- button component -->
+                    <button id="js-ripple-btn2" class="button form_box styl-material btn_edit2">
+                     <img src="menu_images/icon3.png">
+                     <p class="icon_cont">   EDIT SUBJECT LIST</p>
+                        <svg class="ripple-obj" id="js-ripple2">
+                            <use height="100" width="100" xlink:href="#ripply-scott2" class="js-ripple2"></use>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+			   			 
+			<div class="row col-md-offset-12 ">&nbsp;</div>
+
+			
+   
+		</div>
+		</div>
+	</div>
+
+</div>
+
+
+<script src="js/ripple-config.js"></script>
+
+
+
+</div> 
+
+
+			
+	
+		 	
---
+++ uploads.php
@@ -0,0 +1,6 @@
+<?php
+	include_once("includes/header.php");
+    include_once("includes/admin_menu.php");
+	include_once("includes/uploads_content.php");	
+	include_once("includes/footer.php");
+?>
